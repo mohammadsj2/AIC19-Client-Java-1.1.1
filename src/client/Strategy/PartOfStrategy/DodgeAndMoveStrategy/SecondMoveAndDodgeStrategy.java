8728dec190be1de1d4eb16b1dbc7db016d66eb60
Mohammad.Sj <mohammad.sj2@gmail.com>
2019-03-13 10:13:52 +0330

    memory of bfs

--- src/client/Strategy/PartOfStrategy/DodgeAndMoveStrategy/SecondMoveAndDodgeStrategy.java
+++ src/client/Strategy/PartOfStrategy/DodgeAndMoveStrategy/SecondMoveAndDodgeStrategy.java
@@ -1,20 +1,32 @@
 package client.Strategy.PartOfStrategy.DodgeAndMoveStrategy;
 
 import client.Exception.NotEnoughApException;
-import client.model.Cell;
-import client.model.Hero;
-import client.model.World;
+import client.Strategy.Tools.BFS;
+import client.model.*;
 
 import java.util.ArrayList;
 
-public class SecondMoveAndDodgeStrategy extends FirstMoveAndDodgeStrategy{
-    public SecondMoveAndDodgeStrategy(int maxAp) {
+public class SecondMoveAndDodgeStrategy extends FirstMoveAndDodgeStrategy {
+    public static final int NUMBER_OF_MOVE_PHASES = 6;
+    BFS bfs;
+
+    public SecondMoveAndDodgeStrategy(int maxAp, BFS bfs) {
         super(maxAp);
+        this.bfs = bfs;
     }
 
     @Override
     boolean betterToWait(World world, Hero hero, Cell targetCell) {
         return super.betterToWait(world, hero, targetCell);
+    }
+
+    private Pair<Cell, Boolean> whatToDo(World world, Hero hero, Cell targetCell) {
+        Ability dodgeAbility = hero.getDodgeAbilities()[0];
+        int[][][] distance = bfs.getDistance(targetCell, dodgeAbility);
+        int range=Math.max(dodgeAbility.getRange(), NUMBER_OF_MOVE_PHASES);
+        ArrayList<Cell> cells=getARangeOfCellsThatIsNotWall(world,hero.getCurrentCell(),range);
+
+        return null;
     }
 
     @Override
@@ -30,5 +42,10 @@ public class SecondMoveAndDodgeStrategy extends FirstMoveAndDodgeStrategy{
         for (int i = 0; i < 4; i++) {
             dodgeAHero(world, heroes[i], targetCells.get(i));
         }
+    }
+
+    @Override
+    public void moveTurn(World world) throws NotEnoughApException {
+        super.moveTurn(world);
     }
 }
