95075440e76aa9fd26b814b744231f9b1435f4c5
alirezatwk <onlyappleapp@gmail.com>
2019-03-14 08:03:01 +0330

    Third Generation of Attack Strategy (3G)

--- src/client/Strategy/PartOfStrategy/AttackStrategy/FirstLinearAttackStrategy.java
+++ src/client/Strategy/PartOfStrategy/AttackStrategy/FirstLinearAttackStrategy.java
@@ -19,7 +19,7 @@ public class FirstLinearAttackStrategy extends PartOfStrategy {
         Hero hero = world.getHero(heroId);
         super.actionTurn(world);
         Ability attackAbility = hero.getOffensiveAbilities()[0];
-        Cell targetCell1 = getCellWithMostJoneKamShodeForNotLinearAbilities(world, hero.getCurrentCell(), attackAbility.getName(), null);
+        Cell targetCell1 = getCellWithMostJoneKamShode(world, hero.getCurrentCell(), attackAbility.getName(), null, true);
         if (targetCell1 != null)
             castAbility(world, hero, targetCell1, attackAbility.getName());
     }
