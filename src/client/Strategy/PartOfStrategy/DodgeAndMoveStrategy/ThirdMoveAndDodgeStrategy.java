1eadf243ba95a65ab2c64350a826fe7ebfcfc0ef
Mohammad.Sj <mohammad.sj2@gmail.com>
2019-03-14 10:28:54 +0330

    upgrade ThirdMoveAndDodgeStrategy

--- src/client/Strategy/PartOfStrategy/DodgeAndMoveStrategy/ThirdMoveAndDodgeStrategy.java
+++ src/client/Strategy/PartOfStrategy/DodgeAndMoveStrategy/ThirdMoveAndDodgeStrategy.java
@@ -9,7 +9,7 @@ import client.model.World;
 
 import java.util.ArrayList;
 
-public class ThirdMoveAndDodgeStrategy extends SecondMoveAndDodgeStrategy{
+public class ThirdMoveAndDodgeStrategy extends SecondMoveAndDodgeStrategy {
 
     public ThirdMoveAndDodgeStrategy(int maxAp, BFS bfs) {
         super(maxAp, bfs);
@@ -21,7 +21,7 @@ public class ThirdMoveAndDodgeStrategy extends SecondMoveAndDodgeStrategy{
             return targetZoneCells;
 
         int rangeOfBomb = world.getAbilityConstants(AbilityName.BLASTER_BOMB).getAreaOfEffect();
-        ArrayList<Pair<Pair<Integer,Boolean>, ArrayList<Cell>>> toSort = new ArrayList();
+        ArrayList<Pair<Pair<Integer, Boolean>, ArrayList<Cell>>> toSort = new ArrayList();
         for (int minimumDistance = rangeOfBomb * 2 + 1; minimumDistance >= 2; minimumDistance--) {
             boolean flag = false;
             for (int t = 0; t < 30; t++) {
@@ -29,7 +29,7 @@ public class ThirdMoveAndDodgeStrategy extends SecondMoveAndDodgeStrategy{
                     ArrayList<Cell> rndTargetZonesByMinimumDistance =
                             getRandomHeroTargetZonesByMinimumDistance(world, minimumDistance);
                     int maximumDistance = getMaximumDistance(world, rndTargetZonesByMinimumDistance);
-                    boolean twoOfThemIsInALine = isTwoOfThemIsInALine(rndTargetZonesByMinimumDistance);
+                    boolean twoOfThemIsInALine = isTwoOfThemIsInALineOfShadow(world, rndTargetZonesByMinimumDistance);
                     toSort.add(new Pair<>(new Pair<>(maximumDistance, twoOfThemIsInALine), rndTargetZonesByMinimumDistance));
                     flag = true;
                 } catch (CantFindRandomTargetZone ignored) {
@@ -41,13 +41,13 @@ public class ThirdMoveAndDodgeStrategy extends SecondMoveAndDodgeStrategy{
             }
         }
         toSort.sort((o1, o2) -> {
-            if(o1.getFirst().getSecond()!=o2.getFirst().getSecond()){
-                if(!o1.getFirst().getSecond()){
+            if (o1.getFirst().getSecond() != o2.getFirst().getSecond()) {
+                if (!o1.getFirst().getSecond()) {
                     return -1;
                 }
                 return 1;
             }
-            return o1.getFirst().getFirst()-o2.getFirst().getFirst();
+            return o1.getFirst().getFirst() - o2.getFirst().getFirst();
         });
         targetZoneCells = toSort.get(0).getSecond();
         return targetZoneCells;
@@ -62,10 +62,16 @@ public class ThirdMoveAndDodgeStrategy extends SecondMoveAndDodgeStrategy{
         }
         return ans;
     }
-    private boolean isTwoOfThemIsInALine(ArrayList<Cell> targetZones){
+
+    private boolean isTwoOfThemIsInALineOfShadow(World world, ArrayList<Cell> targetZones) {
         for (Cell cell : targetZones) {
             for (Cell cell1 : targetZones) {
-                if(cell.getRow()==cell1.getRow() || cell.getColumn()==cell1.getColumn())
+                int range = world.getAbilityConstants(AbilityName.SHADOW_SLASH).getRange();
+                int manhattanDistance = world.manhattanDistance(cell, cell1);
+                if (manhattanDistance > range) {
+                    continue;
+                }
+                if (cell.getRow() == cell1.getRow() || cell.getColumn() == cell1.getColumn())
                     return true;
             }
         }
